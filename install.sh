665eb21a2af6ece5c3d562040ea3c1d1d35ed584
Dan Kadera <me@subsonic.cz>
2022-11-09 14:54:36 +0100

    installer script, yay

--- install.sh
+++ install.sh
@@ -25,10 +25,15 @@ fi
 umask 027
 
 cd "${tmp}"
-wget -O adm.zip "http://github.com/jahudka/site-admin/archive/main.zip"
+
+echo "Downloading latest SiteAdmin source..."
+wget -O adm.zip "https://github.com/jahudka/site-admin/archive/main.zip"
+
+echo "Unpacking..."
 unzip -oq adm.zip
 
 if [[ ! -e "${install_dir}" ]]; then
+	echo "Scaffolding installation directory..."
 	mkdir -p "${install_dir}"
 	site-admin-main/bin/adm scaffold "${install_dir}"
 
@@ -37,15 +42,24 @@ if [[ ! -e "${install_dir}" ]]; then
 		-v node="$( site-admin-main/bin/adm resolve /opt/node 2>/dev/null | sed -Ee 's/^v|\..+$//g' )"
 fi
 
+echo "Installing new release..."
 mv site-admin-main "${install_dir}/releases/${version}"
 ln -s "${install_dir}/shared/config.env" "${install_dir}/releases/${version}/config.env"
 ln -s "${install_dir}/releases/${version}" "${install_dir}/current.new"
 mv -fT "${install_dir}/current.new" "${install_dir}/current"
 
+echo "Installing 'adm' command symlink..."
 ln -s "${install_dir}/current/bin/adm" /usr/local/bin/adm.new
 mv -fT /usr/local/bin/adm.new /usr/local/bin/adm
+
+echo "Cleaning up..."
+
+cd "${install_dir}"
+rm -rf "${tmp}"
 
 find "${install_dir}/releases" -mindepth 1 -maxdepth 1 -type d -regex '.+/[0-9][^/]+' \
 	| sort -n \
 	| head -n -3 \
 	| xargs -rd '\n' rm -rf
+
+echo "All finished!"
--- resources/config.tpl.sh
+++ resources/config.tpl.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
+cat << EOT
 # This file will be sourced by all commands. You can set up some things here.
 
 # Default PHP and Node versions.
@@ -13,3 +16,4 @@ DEFAULT_NODE_VERSION=${node}
 # Note that only DNS plugins are supported - webroot authentication is not!
 # Required when creating sites with SSL enabled.
 #CERTBOT_AUTHENTICATOR=dns-active24
+EOT
